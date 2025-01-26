e4016147fee279ce00b31e1c66c3bfd2455a1ee1
nipuana <nipuanarimal@gmail.com>
2025-01-26 23:16:40 +0545

    added OrderModel

---
+++ model/OrderModel.js
@@ -0,0 +1,34 @@
+const { DataTypes, STRING } = require("sequelize");
+const sequelize = require("../database/FarmHelp_database");
+
+const Orders = sequelize.define("Orders", {
+   id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+   },
+   orderQuantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+   },
+   Price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+  },
+  order_Date: {
+        type: DataTypes.INTEGER,
+    
+  },
+  address:{
+    type: STRING
+  },
+  status: {
+        type: DataTypes.INTEGER,
+    
+  },
+
+
+});
+
+module.exports = Orders;
+
