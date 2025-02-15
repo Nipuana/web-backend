90305b27d63186b0a3a62dcc1a67689c3b9c18f4
nipuana <nipuanarimal@gmail.com>
2025-02-15 22:34:00 +0545

    made changes in deleteCategory route of categoryRoute because there was a missing (/)

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -9,7 +9,7 @@ const categoryController = require('../controllers/categoryController')
 router.get('/view_category',categoryController.getCategory)
 router.post('/create_category',categoryController.createCategory)
 
-router.put('/update_category',categoryController.updateCategory)
-router.delete('delete_category',categoryController.deleteCategory)
-
+router.put('/update_category/:id',categoryController.updateCategory)
+router.delete('/delete_category/:id',categoryController.deleteCategory)
+    
 module.exports = router;
